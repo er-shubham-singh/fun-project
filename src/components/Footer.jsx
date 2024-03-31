6854148f8652ff8106b03c7e1cfcfa2a59bea007
er-shubham-singh <165591109+er-shubham-singh@users.noreply.github.com>
2024-04-01 02:37:30 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+   <div>
+   <h1>TechyStar</h1>
+    <p>@All right reserved</p>
+   </div>
+
+   <div>
+    <h5>Follow US</h5>
+    <div>
+        <a href="https://www.instagram.com/mrshubham_singh?igsh=MWE0dmxhMjdtMGdhNQ==" target={ "blank"}>Instagram</a>
+    </div>
+   </div>
+   </footer>
+  )
+}
+
+export default Footer;
